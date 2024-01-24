c02562f8cf2e2eb002c19a076b9372542f43a0cb
Kseniia Erina <moraksu@gmail.com>
2024-01-24 16:06:08 +0400

    add HW2

---
+++ src/Seminar02HW/ActorBehaviour.java
@@ -0,0 +1,8 @@
+package Seminar02HW;
+
+public interface ActorBehaviour {
+    void setMakeOrder(boolean makeOrder);
+    void setTakeOrder(boolean takeOrder);
+    boolean isMakeOrder();
+    boolean isTakeOrder();
+}
